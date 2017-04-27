3342c599f3a3df9e60380d44952133a5c0f08d71
Sven Haadem <svenhaadem@gmail.com>
2017-04-27 22:10:40 +0200

    Update upi.sh

--- upi.sh
+++ upi.sh
@@ -42,6 +42,7 @@ dir="$(dirname "$0")"
 . $dir/functions/py_pck
 . $dir/functions/github
 . $dir/functions/ml
+. $dir/functions/rep
 
 #----- Fancy Messages -----#
 show_error(){
@@ -84,6 +85,7 @@ function main {
 		github      'GITHUB:       Clone and install applications from GitHub' \
 		ml          'ML:           Setup a Machine Learning enviroment' \
 		py_pck      'PYTHON:       Install Python and Python packages' \
+		rep      'PYTHON:       Install repository packages' \
 		development 'TOOLS:        Install preferred development tools' \
 		update	    'UPDATE:       Perform system update' \
 		3>&1 1>&2 2>&3)
